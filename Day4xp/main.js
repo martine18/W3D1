899b931b4b3962537d80f6fff1c7bb95d57418fd
Martine <89096155+martine18@users.noreply.github.com>
2023-02-05 01:34:44 -0800

    Add files via upload

---
+++ Day4xp/main.js
@@ -0,0 +1,62 @@
+
+generateColors()
+let currentColor
+listenerMain()
+let isMousePressed = false
+
+function listenerMain() {
+    const main = document.getElementById("main")
+    main.addEventListener("mousedown", handleClick)
+    main.addEventListener("mousemove", handleMove)
+    main.addEventListener("mouseup", handleMouseRelease)
+
+}
+function handleMouseRelease(){
+    isMousePressed = false
+}
+
+function handleClick() {
+    if (currentColor == null) return
+    isMousePressed = true
+}
+
+function handleMove(e){
+if (isMousePressed) return 
+console.log("coordinates:" , e-x, e-y)
+const hoveredDiv = document.elementsFromPoint(e.x,e.y)
+hoveredDiv.style.backgroundColor = currentColor
+}
+
+
+function generateColors() {
+    const colorDivs = document.querySelectorAll(".color")
+    for(const colorDiv of colorDivs) {
+        const randomColor = generateColors()
+        colorDiv.style.backgroundColor = randomColor
+        colorDiv.dataset.color = randomColor
+        colorDiv.addEventListener("click", setCurrentColor)
+    }
+}
+
+function generateColors(){
+    const hex = Math.floor(Math.random() * 16777215).toString(16)
+    return "#" + hex
+}
+
+function setCurrentColor(e) {
+   currentColor = e.target.dataset.color
+   currentColor = chosenColor
+   const button = document.querySelector(".clear button")
+   button.style.backgroundColor = chosenColor
+}
+
+
+
+
+
+
+
+
+
+
+
